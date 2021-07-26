d561cd751efb8deeadf149ab595a9c9f2027de06
Simon Kornél <simon.kornel@mikrum.hu>
2021-07-26 10:25:09 +0200

    better x0 calculation for logistic error function

--- ibello-jmeter-plugin/src/main/java/hu/ibello/functions/LogisticErrorFunction.java
+++ ibello-jmeter-plugin/src/main/java/hu/ibello/functions/LogisticErrorFunction.java
@@ -4,7 +4,12 @@ public class LogisticErrorFunction extends Logistic5Function implements X0Functi
 
 	@Override
 	public double getX0() {
-		return getC();
+		double x0 = getC();
+		double delta = x0 / 1000;
+		while (value(x0) > 0.01) {
+			x0 -= delta;
+		}
+		return x0;
 	}
 	
 	@Override
